1e2e6bd4c5cd94e9d97cca0b9845ba71a825d033
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-20 15:45:17 +0900

    Feat: NextJS Link, useRouter 학습

---
+++ components/NavBar.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link href="/">
+        <a style={{ color: router.pathname === "/" ? "tomato" : "teal" }}>
+          Home
+        </a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === "/about" ? "tomato" : "teal" }}>
+          About
+        </a>
+      </Link>
+    </nav>
+  );
+}
