469c91a1129be56a912d50bca17fb2dcd1a512d5
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-21 21:06:26 +0900

    Feat: .module.css 연습

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,26 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "tomato" : "teal" }}>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
           Home
         </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "tomato" : "teal" }}>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/about" ? styles.active : ""
+          }`}
+        >
           About
         </a>
       </Link>
