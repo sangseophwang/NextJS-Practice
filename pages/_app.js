a0ea64354e7975ec7ca65937a601cb651a19c0fc
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-04 22:38:44 +0900

    feat: custom app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
-export default function App({ Component, pageProps }) {
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NavBar />
       <Component {...pageProps} />
-      <style jsx global>
-        {`
-          a {
-            color: white;
-          }
-        `}
-      </style>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import NavBar from "../components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <h1>Home</h1>
+      <h1 className="active">Home</h1>
     </div>
   );
 }
